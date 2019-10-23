fb3c8bc562953bd46f998eb40e70654bec5e4fde
Adam <adam.ferguson124@gmail.com>
2019-10-23 18:15:34 -0400

    render the two apps

---
+++ src/react/root.component.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+const App = () => <h1>If you're reading this, React is up and running </h1>
+
+export default App
---
+++ src/vue/main.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+      <h1>The Vue is Spectavular </h1>
+  </div>
+</template>
